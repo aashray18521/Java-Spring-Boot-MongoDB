685846960754fba0d583a264ed3578e704344aa2
Aashray Jain <aashray.jain@oracle.com>
2025-12-16 14:33:44 +0530

    Added .xml to .gitignore
    Added Swagger Documentation for all Mappings

--- src/main/java/com/jain/joblisting/controller/JobPostController.java
+++ src/main/java/com/jain/joblisting/controller/JobPostController.java
@@ -2,6 +2,9 @@ package com.jain.joblisting.controller;
 
 import com.jain.joblisting.model.JobPost;
 import com.jain.joblisting.repository.JobPostRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@Tag(name = "Job Posts", description = "APIs for managing Job Posts")
 public class JobPostController {
 
     @Autowired
@@ -24,11 +28,29 @@ public class JobPostController {
         response.sendRedirect("/swagger-ui.html");
     }
 
+    @Operation(
+            summary = "Get all job posts",
+            description = "Fetches the complete list of job posts from DB",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrueved job posts.")
+            }
+    )
     @GetMapping("/jobPosts")
     public List<JobPost> getAllJobPosts() {
         return jobPostRepository.findAll();
     }
 
+    @Operation(
+            summary = "Create a new Job Post record in the DB",
+            description = "Create a new Job Post record in the DB using user input job post",
+//            requestBody =  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+//                    description = "Job post payload",
+//                    required = true
+//            ),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Job post created successfully")
+            }
+    )
     @PostMapping("/jobPosts")
     public JobPost createJobPost(@RequestBody JobPost jobPost) {
         return jobPostRepository.save(jobPost);
